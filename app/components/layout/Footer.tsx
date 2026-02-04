267b3996ebc20dba7a60f4f1670e5757b8a2a4a3
Divyansh <divyanshbfc@gmail.com>
2026-02-04 13:01:11 +0530

    container fixed footer

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
             className="text-[#334155] py-20 px-4"
             style={{ background: 'linear-gradient(0deg, #CFE4F3 0%, #ffffff 68.29%)' }}
         >
-            <div className="container mx-auto">
+            <div className="container mx-auto px-5">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 mb-16">
 
                     {/* Column 1: Compliance & Info */}
