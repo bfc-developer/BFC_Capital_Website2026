59d9a45d701aa59fef397049a75c0236cd2c069a
Divyansh <divyanshbfc@gmail.com>
2026-02-24 15:57:31 +0530

    nav bar updated by santosh sir

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -2,22 +2,15 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { Menu, X } from "lucide-react";
+import { Menu, X, ChevronDown } from "lucide-react";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMoreOpen, setIsMoreOpen] = useState(false);
+
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-  const pathname = usePathname();
-  const navLinks = [
-    { name: "Home", href: "/" },
-    { name: "Mutual Funds", href: "/mutual-funds" },
-    { name: "Financial Planning", href: "/financial-planning" },
-    { name: "About", href: "/about" },
-    { name: "More", href: "/more" },
-    { name: "Download App", href: "/download-app" },
-  ];
+  const toggleMore = () => setIsMoreOpen(!isMoreOpen);
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -26,44 +19,72 @@ const Navbar = () => {
   return (
     <nav className="sticky top-0 z-[10] w-full bg-white shadow-sm font-sans">
       <div className="container mx-auto px-4 md:px-6 h-20 flex items-center justify-between">
-        {/* Logo Section */}
+
+        {/* Logo */}
         <div className="flex-shrink-0 flex items-center gap-2">
-          <div className="flex items-center gap-2">
-            <Link href="/" onClick={scrollToTop}>
-              <Image
-                src="/Logo/CAPLOGO.svg"
-                alt=""
-                width={155}
-                height={155}
-              />
-            </Link>
-          </div>
+          <Link href="/" onClick={scrollToTop}>
+            <Image
+              src="/Logo/CAPLOGO.svg"
+              alt=""
+              width={155}
+              height={155}
+            />
+          </Link>
         </div>
 
         {/* Desktop Navigation */}
-        <div className="hidden font-semibold lg:flex items-center space-x-5 xl:space-x-8">
-          {navLinks.map((link) => {
-            const isActive = pathname === link.href;
+        <div className="hidden xl:flex items-center space-x-5 xl:space-x-[28px] relative">
 
-            return (
-              <Link
-                key={link.name}
-                href={link.href}
-                className={`font-inter transition-colors
-              ${isActive
-                    ? "text-[#001EFE] font-semibold"
-                    : "text-gray-700 hover:text-[#001EFE]"
-                  }
-            `}
-              >
-                {link.name}
-              </Link>
-            );
-          })}
+          <Link href="/" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"> Home</Link>
+          <Link href="/mutual-funds" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"> Mutual Funds</Link>
+          <Link href="/sif" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"> SIF</Link>
+          <Link href="/financial-planning" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"> Financial Planning</Link>
+          {/* More Dropdown */}
+          <div className="relative">
+            <button
+              onClick={toggleMore}
+              className="flex items-center gap-1 text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"
+            >
+              More
+              {/* More <ChevronDown size={16} /> */}
+            </button>
+
+            {isMoreOpen && (
+              <div className="absolute top-8 left-[-5rem] bg-white shadow-lg border border-gray-100 rounded-md py-2 w-90 z-50 p-4">
+                <div className="px-md-4 mb-2">
+                  <Link href="/about" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"> About </Link>
+                  <p className="text-[#4B4949] text-[11px] font-base font-inter transition-colors text-[13px] leading-tight">Where research meets responsible advice.</p>
+                </div>
+                <div className="px-md-4 mb-2">
+                  <Link href="/financial-calculators" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"
+                  > Financial Calculators </Link>
+                  <p className="text-[#4B4949] text-[11px] font-base font-inter transition-colors text-[13px] leading-tight">Because good decisions need good numbers.</p>
+                </div>
+                <div className="px-md-4 mb-2">
+                  <Link href="/blog" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"
+                  > Blog </Link>
+                  <p className="text-[#4B4949] text-[11px] font-base font-inter transition-colors text-[13px] leading-tight">Insights that help you invest with confidence.</p>
+                </div>
+                <div className="px-md-4 mb-2">
+                  <Link href="/career" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"
+                  > Career </Link>
+                  <p className="text-[#4B4949] text-[11px] font-base font-inter transition-colors text-[13px] leading-tight">Do meaningful work. Build lasting value.</p>
+                </div>
+                <div className="px-md-4 mb-2">
+                  <Link href="/contact-us
+							" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"
+                  > Contact Us
+                  </Link>
+                  <p className="text-[#4B4949] text-[11px] font-base font-inter transition-colors text-[13px] leading-tight">Start a smarter financial conversation.</p>
+                </div>
+              </div>
+            )}
+          </div>
+          <Link href="/download-app" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"> Download App</Link>
         </div>
 
-        {/* Action Buttons (Desktop) */}
-        <div className="hidden lg:flex items-center gap-10 md:gap-10 xl:gap-20">
+        {/* Desktop Buttons */}
+        <div className="hidden xl:flex items-center gap-10 md:gap-10 xl:gap-20">
           <a
             href="https://app.prodigypro.co.in/"
             className="inline-block px-5 py-1.5 bg-bfc-green text-white text-sm font-inter rounded-full hover:bg-opacity-90 transition-opacity whitespace-nowrap shadow-md hover:shadow-lg"
@@ -71,22 +92,19 @@ const Navbar = () => {
             Login / Sign-up
           </a>
 
-          <div className="flex items-center gap-1">
-            <a href="https://app.prodigypro.co.in/">
-              <Image
-                src="/Logo/ProdigyPro_horizontal.svg"
-                alt=""
-                width={145}
-                height={145}
-              />
-            </a>
-
-          </div>
+          <a href="https://app.prodigypro.co.in/">
+            <Image
+              src="/Logo/ProdigyPro_horizontal.svg"
+              alt=""
+              width={145}
+              height={145}
+            />
+          </a>
         </div>
 
         {/* Mobile Menu Button */}
         <button
-          className="lg:hidden p-2 text-gray-600 hover:text-[#001EFE] transition-colors focus:outline-none"
+          className="xl:hidden p-2 text-gray-600 hover:text-bfc-blue transition-colors focus:outline-none"
           onClick={toggleMenu}
           aria-label="Toggle menu"
         >
@@ -94,23 +112,82 @@ const Navbar = () => {
         </button>
       </div>
 
-      {/* Mobile Menu Overlay */}
+      {/* Mobile Menu */}
       {isMenuOpen && (
-        <div className="lg:hidden absolute top-20 left-0 w-full bg-white shadow-lg border-t border-gray-100 py-4 px-4 flex flex-col gap-4 animate-in slide-in-from-top-5 duration-200">
-          {navLinks.map((link) => (
-            <Link
-              key={link.name}
-              href={link.href}
-              className="text-gray-800 font-inter text-medium py-2 border-b border-gray-50 hover:text-[#001EFE] hover:pl-2 transition-all"
-              onClick={() => setIsMenuOpen(false)}
+        <div className="xl:hidden absolute top-20 left-0 w-full bg-white shadow-lg border-t border-gray-100 py-4 px-4 flex flex-col gap-4 animate-in slide-in-from-top-5 duration-200">
+
+
+          <Link href="/" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-inter border-b border-gray-50 hover:pl-2 transition-all"
+          >
+            Home
+          </Link>
+          <Link href="/mutual-funds" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-inter border-b border-gray-50 hover:pl-2 transition-all"
+          >
+            Mutual Funds
+          </Link>
+          <Link href="/sif" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-inter border-b border-gray-50 hover:pl-2 transition-all"
+          >
+            SIF
+          </Link>
+          <Link href="/financial-planning" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-inter border-b border-gray-50 hover:pl-2 transition-all"
+          >
+            Financial Planning
+          </Link>
+
+          {/* Mobile More Dropdown */}
+          <div>
+            <button
+              onClick={toggleMore}
+              className="w-full flex justify-between items-center text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-inter border-b border-gray-50"
             >
-              {link.name}
-            </Link>
-          ))}
-          <div className="mt-4 flex flex-col gap-4">
-            <button className="w-full py-2 bg-bfc-green text-white font-inter rounded-lg shadow-md">
-              Login / Sign-up
+              More
+              {/* More <ChevronDown size={16} /> */}
             </button>
+
+            {isMoreOpen && (
+              <div className="pl-4 flex flex-col gap-2 mt-2">
+                <div className="px-md-4 mb-1">
+                  <Link href="/about" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"> About </Link>
+                  <p className="text-[#4B4949] text-[11px] font-base font-inter transition-colors text-[13px]">Where research meets responsible advice.</p>
+                </div>
+                <div className="px-md-4 mb-1">
+                  <Link href="/financial-calculators" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"
+                  > Financial Calculators </Link>
+                  <p className="text-[#4B4949] text-[11px] font-base font-inter transition-colors text-[13px]">Because good decisions need good numbers.</p>
+                </div>
+                <div className="px-md-4 mb-1">
+                  <Link href="/blog" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"
+                  > Blog </Link>
+                  <p className="text-[#4B4949] text-[11px] hover:text-[#001EFE] font-base font-inter transition-colors text-[13px]">Insights that help you invest with confidence.</p>
+                </div>
+                <div className="px-md-4 mb-1">
+                  <Link href="/career" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"
+                  > Career </Link>
+                  <p className="text-[#4B4949] text-[11px] font-base font-inter transition-colors text-[13px]">Do meaningful work. Build lasting value.</p>
+                </div>
+                <div className="px-md-4 mb-1">
+                  <Link href="/contact-us
+							" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-base font-inter transition-colors"
+                  > Contact Us
+                  </Link>
+                  <p className="text-[#4B4949] text-[11px] font-base font-inter transition-colors text-[13px]">Start a smarter financial conversation.</p>
+                </div>
+              </div>
+            )}
+          </div>
+
+          <Link href="/download-app" className="text-[#4B4949] font-[500] text-[15px] hover:text-[#001EFE] font-inter border-b border-gray-50 hover:pl-2 transition-all"
+          >
+            Download App
+          </Link>
+
+          <div className="mt-4">
+            <a
+              href="https://app.prodigypro.co.in/"
+              className="w-full block text-center py-2 bg-bfc-green text-white font-inter rounded-lg shadow-md"
+            >
+              Login / Sign-up
+            </a>
           </div>
         </div>
       )}
