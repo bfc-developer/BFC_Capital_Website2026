3ac13a094b3df9f4e0468ac962e1700060c7785f
Divyansh <divyanshbfc@gmail.com>
2026-03-25 18:04:20 +0530

    changes by akash sir

--- app/components/financial-planning/FinancialPlanning.tsx
+++ app/components/financial-planning/FinancialPlanning.tsx
@@ -286,14 +286,15 @@ export default function FinancialPlanning() {
                     <p className="mx-auto mt-4 md:mt-8 mb-4 text-[18px] md:text-[24px] font-bold text-[#44475B] mb-[20px]">
                         To Conclude
                     </p>
-                    <p className='text-[#44475B] text-[15px] md:text-[17px] leading-relaxed font-inter mx-auto mb-9'>
+                    <p className='text-[#44475B] text-[15px] md:text-[17px] leading-relaxed font-inter mx-auto mb-4'>
                         Financial planning isn't just about money. <br />
                         It's about peace of mind, confidence, and knowing that you're prepared – whatever life throws at you. <br />
                         If you're ready to build a clear financial roadmap, <b>BFC Capital</b> is here to help.
                     </p>
-                    <Link href="https://app.prodigypro.co.in/dashboard" className="bg-[#04B488] text-white px-6 py-2 rounded-md hover:bg-[#008f45] transition duration-300 mt-10">
-                        Take the first step now!
-                    </Link>
+                    <BookSessionButton
+                        buttonText="Take the first step now!"
+                        className="bg-[#04B488] text-white px-6 py-2 rounded-md hover:bg-[#008f45] transition duration-300 mt-5"
+                    />
                 </div>
 
                 <div className='container mx-auto px-5 md:px-10 lg:px-20'>
@@ -456,12 +457,13 @@ export default function FinancialPlanning() {
                     <p className="mx-auto mt-4 md:mt-8 mb-4 text-[18px] md:text-[24px] font-bold text-[#44475B] mb-[20px]">
                         To Conclude
                     </p>
-                    <p className='text-[#44475B] text-[15px] md:text-[17px] leading-relaxed font-inter mx-auto mb-9'>
+                    <p className='text-[#44475B] text-[15px] md:text-[17px] leading-relaxed font-inter mx-auto mb-4'>
                         Your financial needs change with time and your strategy must evolve too.
                     </p>
-                    <Link href="https://app.prodigypro.co.in/dashboard" className="bg-[#04B488] text-white px-6 py-2 rounded-md hover:bg-[#008f45] transition duration-300 mt-5">
-                        Invest now!
-                    </Link>
+                    <BookSessionButton
+                        buttonText="Invest now!"
+                        className="bg-[#04B488] text-white px-6 py-2 rounded-md hover:bg-[#008f45] transition duration-300 mt-5"
+                    />
                 </div>
 
                 <div className='container mx-auto px-5 md:px-10 lg:px-20 mb-8'>
